098b00c7b9ee8276ce1acc3be90c3b8c472bfcfa
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2022-10-04 15:20:14 -0400

    Time: 2 ms (64.31%), Space: 49.3 MB (23.03%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -4,17 +4,10 @@ class Solution
     {
         int[] left = new int[height.length], right = new int[height.length];
         int sum = 0, max = height[0];
-        for(int i = 0; i < height.length; i++) 
-        {
-            left[i] = Math.max(max, height[i]);
-            max = left[i];
-        }
-        max = height[height.length-1];
-        for(int i = height.length-1; i >= 0; i--)
-        {
-            right[i] = Math.max(max, height[i]);
-            max = right[i];
-        }
+        left[0] = height[0];
+        right[height.length-1] = height[height.length-1];
+        for(int i = 1; i < height.length; i++) left[i] = Math.max(left[i-1], height[i]);
+        for(int i = height.length-2; i >= 0; i--) right[i] = Math.max(right[i+1], height[i]);
         for(int i = 0; i < height.length; i++)
         {
             int temp = Math.min(left[i], right[i]) - height[i];
