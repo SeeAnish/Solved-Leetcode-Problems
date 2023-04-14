181642fe2b664e148c7183bfef37af46acb38f29
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-04-14 05:29:50 -0400

    Time: 2 ms (87.79%), Space: 59.5 MB (26.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,13 +2,15 @@ class Solution
 {
     public int maxProfit(int[] prices) 
     {
-        if(prices.length == 0 || prices == null) return 0;
+        if(prices == null || prices.length == 0) return 0;
         int current = Integer.MAX_VALUE, max = 0;
         for(int i = 0; i < prices.length; i++)
         {
             if(prices[i] < current) current = prices[i];
-            else if(prices[i] - current > max) max = prices[i] - current;
+            max = Math.max(max, prices[i] - current);
         }
         return max;
     }
 }
+//Time complexity: O(N)
+//Space complexity: O(1)
