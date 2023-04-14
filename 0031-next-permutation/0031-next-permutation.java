0f71877d332fcf2e2958e5124cb5ef2e627bd125
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-04-14 04:52:20 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (77.45%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public void nextPermutation(int[] nums) 
+    {
+        int pivot = nums.length - 1;
+        while(pivot >=1 && nums[pivot] <= nums[pivot-1]) pivot--;
+        if(pivot != 0)
+        {
+            int i = nums.length-1;
+            while(nums[i] <= nums[pivot-1]) i--;
+            swap(nums, i, pivot-1);
+        }
+        int left = pivot;
+        int right = nums.length -1;
+        while(left < right)
+        {
+            swap(nums, left++, right--);
+        }
+    }
+    private void swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+//Time complexity:
+//Space complexity:
