ae4e8b3c48ced58db92cb7fac3d4d77c245117e5
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-04-25 03:58:26 -0400

    Time: 1 ms (98.79%), Space: 39.7 MB (90.50%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -3,12 +3,12 @@ class Solution
     public int lastStoneWeight(int[] stones) 
     {
         PriorityQueue<Integer> pathar = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i : stones) pathar.offer(i);
+        for (int i : stones) pathar.add(i);
         while (pathar.size() != 1) 
         {
             int stone1 = pathar.poll();
             int stone2 = pathar.poll();
-            pathar.offer(Math.abs(stone1 - stone2));
+            pathar.add(Math.abs(stone1 - stone2));
         }
         return pathar.poll();    
     }
