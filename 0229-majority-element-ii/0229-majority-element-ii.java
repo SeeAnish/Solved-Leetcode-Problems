f7c6dd634f4bf3faabf23f240d89fff02fc39739
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-05-03 18:47:04 -0400

    Time: 13 ms (30.69%), Space: 46 MB (67.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,38 +2,19 @@ class Solution
 {
     public List<Integer> majorityElement(int[] nums) 
     {
-        int candidate1 = Integer.MIN_VALUE, candidate2 = Integer.MIN_VALUE; 
-        int life1 = 0, life2 = 0, c1 = 0, c2 = 0;
         List<Integer> result = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++)
+        if(nums.length == 1)
         {
-            if(nums[i] == candidate1) life1++;
-            else if(nums[i] == candidate2) life2++;
-            else if(life1 == 0) 
-            {
-                candidate1 = nums[i];
-                life1 = 1;
-            }
-            else if(life2 == 0)
-            {
-                candidate2 = nums[i];
-                life2 = 1;
-            }
-            else
-            {
-                life1--;
-                life2--;
-            }
+            result.add(nums[0]);
+            return result;
         }
-        for(int i: nums)
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num: nums)
         {
-            if(candidate1 == i) c1++;
-            if(candidate2 == i) c2++;
+            if(map.containsKey(num)) map.put(num, map.get(num) + 1);
+            else map.put(num, 1);
         }
-        if(c1 > nums.length/3) result.add(candidate1);
-        if(c2 > nums.length/3) result.add(candidate2);
+        for(int i: map.keySet()) if(map.get(i) > nums.length/3) result.add(i);
         return result;
     }
 }
-//Time Complexity: O(N)
-//Space Complexity: O(1)
