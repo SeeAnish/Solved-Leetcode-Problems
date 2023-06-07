79c59a3233e1678646e9adbd84290d43a40d37df
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-06-07 02:03:02 -0400

    Time: 1 ms (53.62%), Space: 54.8 MB (98.44%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,12 @@
+class Solution 
+{
+    public void rotate(int[] nums, int k) 
+    {
+        int[] array = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) array[(i+k)% nums.length] = nums[i];
+        for(int i = 0; i < nums.length; i++) nums[i] = array[i];
+        
+    }
+}
+//Time Complexity: O(n)
+//Space Complexity: O(n)
