1f6d3198483f6d62d3f6cfa14c7f0cf29c79d201
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-07-01 22:59:58 -0400

    Time: 3 ms (43.67%), Space: 44.7 MB (29.36%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,14 +5,12 @@ class Solution
         int count = 0, max = 0;
         for(int i: nums)
         {
-            if(i == 1)
-            {
-                count++;
-                max = Math.max(count, max);
-            }
+            if(i == 1) count++;
             else count = 0;
-            
+            max = Math.max(count, max);
         }
         return max;
     }
 }
+//Time Complexity: O(N);
+//Space Complexity: O(1);
