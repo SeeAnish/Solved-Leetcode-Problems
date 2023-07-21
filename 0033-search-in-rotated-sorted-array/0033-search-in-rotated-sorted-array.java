8a8ff5c1a63c3d55af458e1b88002b761b6c0853
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-07-21 14:18:27 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (45.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,21 +6,19 @@ class Solution
         int left = 0, right = nums.length-1;
         while(left <= right)
         {
-            int mid = left + (right - left)/2;
+            int mid = left + (right -left)/2;
             if(nums[mid] == target) return mid;
             else if(nums[left] <= nums[mid])
             {
-                if(nums[left] <= target && target <= nums[mid]) right = mid - 1;
-                else left = mid+1;
+                if(nums[left] > target || target > nums[mid]) left = mid+1;
+                else right = mid-1;
             }
             else
             {
-                if(nums[mid] <= target && target <= nums[right]) left = mid+1;
-                else right = mid-1;
+                if(nums[mid] > target || target > nums[right]) right = mid - 1;
+                else left = mid + 1;
             }
         }
         return -1;
     }
 }
-//Time Complexity: O(Log N);
-//Space Complexity: O(1)
