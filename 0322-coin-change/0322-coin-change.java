46422f4d99d67150b13ef1284081672df171b9a1
Anish Malhotra <95327601+SeeAnish@users.noreply.github.com>
2023-08-11 13:13:15 -0400

    Time: 16 ms (79.09%), Space: 42.8 MB (97.32%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int coinChange(int[] coins, int amount) 
+    {
+        if(coins.length == 0 || coins == null) return -1;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++)
+            for(int j = 0; j < coins.length; j++)
+                if(coins[j] <= i) 
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+        return amount >= dp[amount] ? dp[amount] : -1;
+    }
+}
+//Time complexity: O(N*amount);
+//Space Complexiy: O(amount);
